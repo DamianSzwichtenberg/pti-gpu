ffdd145330d389f59a6975f90c5232b41591dfb6
Damian Szwichtenberg <dszwichtenberg@habana.ai>
2024-07-13 12:06:32 +0300

    Increase frequency of metric calculations

--- samples/ze_metric_streamer/tool.cc
+++ samples/ze_metric_streamer/tool.cc
@@ -10,14 +10,6 @@
 
 #include "ze_metric_collector.h"
 
-struct MetricResult {
-  uint64_t inst_alu0 = 0;
-  uint64_t inst_alu1 = 0;
-  uint64_t inst_xmx = 0;
-  uint64_t inst_send = 0;
-  uint64_t inst_ctrl = 0;
-};
-
 const uint32_t kInstructionLength = 20;
 
 static ZeMetricCollector* metric_collector = nullptr;
@@ -50,48 +42,18 @@ void SetToolEnv() {
 static MetricResult GetMetricResult() {
   PTI_ASSERT(metric_collector != nullptr);
 
-  std::vector<zet_typed_value_t> report_list =
-    metric_collector->GetReportList();
-  if (report_list.size() == 0) {
-    return MetricResult();
-  }
+  std::vector<MetricResult> metric_list =
+    metric_collector->GetMetricsList();
+  std::cout << "Performed " << metric_list.size() << " samplings." << std::endl;
 
   MetricResult metric_result;
 
-  int inst_alu0_id = metric_collector->GetMetricId("XVE_INST_EXECUTED_ALU0_ALL");
-  PTI_ASSERT(inst_alu0_id > 0);
-  int inst_alu1_id = metric_collector->GetMetricId("XVE_INST_EXECUTED_ALU1_ALL");
-  PTI_ASSERT(inst_alu1_id > 0);
-  int inst_xmx_id = metric_collector->GetMetricId("XVE_INST_EXECUTED_XMX_ALL");
-  PTI_ASSERT(inst_xmx_id > 0);
-  int inst_send_id = metric_collector->GetMetricId("XVE_INST_EXECUTED_SEND_ALL");
-  PTI_ASSERT(inst_send_id > 0);
-  int inst_ctrl_id = metric_collector->GetMetricId("XVE_INST_EXECUTED_CONTROL_ALL");
-  PTI_ASSERT(inst_ctrl_id > 0);
-
-  uint32_t report_size = metric_collector->GetReportSize();
-  PTI_ASSERT(report_size > 0);
-
-  const zet_typed_value_t* report = report_list.data();
-  while (report < report_list.data() + report_list.size()) {
-    PTI_ASSERT(report[inst_alu0_id].type == ZET_VALUE_TYPE_UINT64);
-    uint64_t inst_alu0 = report[inst_alu0_id].value.ui64;
-    PTI_ASSERT(report[inst_alu1_id].type == ZET_VALUE_TYPE_UINT64);
-    uint64_t inst_alu1 = report[inst_alu1_id].value.ui64;
-    PTI_ASSERT(report[inst_xmx_id].type == ZET_VALUE_TYPE_UINT64);
-    uint64_t inst_xmx = report[inst_xmx_id].value.ui64;
-    PTI_ASSERT(report[inst_send_id].type == ZET_VALUE_TYPE_UINT64);
-    uint64_t inst_send = report[inst_send_id].value.ui64;
-    PTI_ASSERT(report[inst_ctrl_id].type == ZET_VALUE_TYPE_UINT64);
-    uint64_t inst_ctrl = report[inst_ctrl_id].value.ui64;
-
-    metric_result.inst_alu0 += inst_alu0;
-    metric_result.inst_alu1 += inst_alu1;
-    metric_result.inst_xmx += inst_xmx;
-    metric_result.inst_send += inst_send;
-    metric_result.inst_ctrl += inst_ctrl;
-
-    report += report_size;
+  for (const auto& metric : metric_list) {
+    metric_result.inst_alu0 += metric.inst_alu0;
+    metric_result.inst_alu1 += metric.inst_alu1;
+    metric_result.inst_xmx += metric.inst_xmx;
+    metric_result.inst_send += metric.inst_send;
+    metric_result.inst_ctrl += metric.inst_ctrl;
   }
 
   return metric_result;
